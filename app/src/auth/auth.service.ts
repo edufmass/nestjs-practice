f6445502dadabaa10cd300ac608ec039f27d2371
mastix <mastix@msi.grid.net.ar>
2023-07-04 20:41:14 -0300

    Check mail unique before saving user

--- app/src/auth/auth.service.ts
+++ app/src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { DatabaseService } from "src/database/database.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable({})
 export class AuthService {
@@ -11,25 +12,36 @@ export class AuthService {
 
     async signup(dto: AuthDto) {
         const hash = await argon.hash(dto.password);
-        const user = await this.db.user.create({
-            data: {
-                email: dto.email,
-                password: hash
-            },
-            select: {
-                id: true,
-                email: true,
-                firstName: true,
-                lastName: true,
-                updatedAt: true,
-                createdAt: true
-            }
-        });
+        try {
+            const user = await this.db.user.create({
+                data: {
+                    email: dto.email,
+                    password: hash
+                },
+                select: {
+                    id: true,
+                    email: true,
+                    firstName: true,
+                    lastName: true,
+                    updatedAt: true,
+                    createdAt: true
+                }
+            });
 
-        return {
-            msg: 'Signed up correctly!',
-            user
-        };
+            return {
+                msg: 'Signed up correctly!',
+                user
+            };
+        } catch(error) {
+            if(error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException(
+                        'Email error, try another.',
+                    );
+                }
+            }
+            throw error;
+        }
     }
 
     login() {
