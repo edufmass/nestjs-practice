1cdbfdd2d274b7a9a385f29a675307bb1d4f0bbe
mastix <mastix@msi.grid.net.ar>
2023-07-04 17:45:39 -0300

    Save user to db and updated models

--- app/src/auth/auth.service.ts
+++ app/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { DatabaseService } from "src/database/database.service";
 import { AuthDto } from "./dto";
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
@@ -8,11 +9,30 @@ export class AuthService {
         private db:DatabaseService
     ) {}
 
-    login() {
-        return { msg: 'You are logged in.' };
+    async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+        const user = await this.db.user.create({
+            data: {
+                email: dto.email,
+                password: hash
+            },
+            select: {
+                id: true,
+                email: true,
+                firstName: true,
+                lastName: true,
+                updatedAt: true,
+                createdAt: true
+            }
+        });
+
+        return {
+            msg: 'Signed up correctly!',
+            user
+        };
     }
 
-    signup(dto: AuthDto) {
-        return { msg: 'Signed up correctly!' };
+    login() {
+        return { msg: 'You are logged in.' };
     }
 }
