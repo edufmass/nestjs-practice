0c3bcc849db77ec2c268dd730b299d0b157ddd7e
edufmass <eduardo@massolini.com.ar>
2023-07-04 23:10:28 -0300

    Added JWT Strategy

---
+++ app/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,13 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(config: ConfigService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: config.get('JWT_SECRET')
+        });
+    }
+}
